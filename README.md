6727b370504769edc045c473becd7a026ce78d2d
SimonsLab <simon.bihlmaier@gmail.com>
2021-05-18 21:50:10 +0200

    update readme, add ui server communication

--- README.md
+++ README.md
@@ -10,36 +10,54 @@ Reading Data:
 https://wiki.openstreetmap.org/wiki/PBF_Format#The_code  
 https://osmcode.org/libosmium/
 
-## Meeting Notes
-Two neighboring Nodes in the water -> We assume there is water in between the nodes
-
-## First Task
-
-Read PBF File and extract coastlines.  
-Output them to the console or display them by converting them to geoJSON.
+## Dependencies
 
 ### libosmiumpbfreader:
 https://github.com/CanalTP/libosmpbfreader
 
-Dependency:  
+Install using apt:  
 
     sudo apt-get install libosmpbf-dev
 
+### libosmiumpbfreader:
+https://github.com/yhirose/cpp-httplib
 
-### Approach
+## Installation
 
-* Create Reader object of pbf
-* Create Handler that reads coastlines
-* Save Coastlines to geoJson (or use data directly)
+### Install Program using CMake
 
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release ..  
+    make -j4
+    make install
+    cd .. 
 
+## Usage
 
-### install execute program
+### Extract Coastlines
+From project root:
 
-cmake -DCMAKE_BUILD_TYPE=Release .
-make 
-make install 
+    ./build/bin/coastlineExtraction data/planet-coastlines.pbf data/planet-coastlines
 
-./bin/osmrouting data/planet-coastline.pbf data/planet-coastlin
+### Create File containing PointInPolygon Queries for Grid-Graph creation
+From project root:
 
+    ./build/bin/pointInPolygon {path to extracted coastlines} {result save location} {number of grid points along longitude} {number of grid points along latitude}
+
+Example:
+
+    ./build/bin/pointInPolygon ./build/bin/planet-coastlines.save data/worldGrid_1415_707.save 1415 707
+
+### Create Adjacency Array from PointInPolygon Queries file
+From project root:
+
+    ./build/bin/buildGraph data/worldGrid_1415_707.save data/wordGraph_1415_707.save
+
+### Start Server
+From project root:
+
+    ./build/bin/server
+
+Visit http://localhost:8080 using the Browser
 
--- src/server.cpp
+++ src/server.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 void loadStatic(std::string& path, std::string& loaded){
     std::ifstream staticFile (path);
@@ -14,6 +15,22 @@ void loadStatic(std::string& path, std::string& loaded){
             }
     }
 }
+
+void generateReponse(double longStart, double latStart, double longGoal, double latGoal, std::string& response){
+    
+    // TODO: fill with real routing response
+    std::vector<double> path {longStart, latStart, longGoal, latGoal};
+    
+    // response has to be in [[lat,long],...] format, so (long, lat) is swapped
+    response += "{\"path\":[";
+    response += "[" + std::to_string(path.at(1)) + "," + std::to_string(path.at(0)) + "]";
+    for(int i=1; i<path.size()/2; ++i){
+        response += ",[" + std::to_string(path.at(2*i+1)) + "," + std::to_string(path.at(2*i)) + "]";
+    
+    }
+    response += "]}";
+}
+    
 
 static std::string page;
 
@@ -29,6 +46,20 @@ int main(void)
 
     svr.Get("/", [](const Request& req, Response& res) {
         res.set_content(page, "text/html");
+    });
+
+    svr.Post("/getRoute", [](const Request& req, Response& res) {
+        double longStart =  std::stod((*req.params.find("longStart")).second);
+        double latStart =   std::stod((*req.params.find("latStart")).second);
+        double longGoal =   std::stod((*req.params.find("longGoal")).second);
+        double latGoal =    std::stod((*req.params.find("latGoal")).second);
+        std::cout << "Route from (" << longStart << ", " << latStart << ") to (" << longGoal << ", " << latGoal << ")\n";
+        
+        std::string response;
+        generateReponse(longStart, latStart, longGoal, latGoal, response);
+        std::cout << response << std::endl;
+        // return geojson with of results path
+        res.set_content(response, "application/json");
     });
 
     // svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
