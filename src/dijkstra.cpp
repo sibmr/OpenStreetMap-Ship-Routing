411274b625ba83da283877baaaf10a74c0434c7c
Marcel <marcel.galuschka@web.de>
2021-05-22 11:44:23 +0200

    add dijkstra with ciorrect distances

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -110,20 +110,27 @@ double latLongDistance(double p1lng, double p1lat, double p2lng, double p2lat){
     return d;
 }
 
-uint64_t nodeDistance(AdjacencyArray &array, uint64_t node1, uint64_t node2){
-
+void nodeIdToLongLat(std::array<double,2> &result, AdjacencyArray &array, uint64_t id){
     const double longStep = (array.longHigh-array.longLow)/(array.width+1);
     const double latStep = (array.latHigh-array.latLow)/(array.height+1);
 
-    double n1_long =    array.longLow   +  ((node1%array.width) * longStep + (longStep/2)) ;
-    double n1_lat =     array.latLow    +  std::floor(node1/array.width) * latStep + (latStep/2);
-    double n2_long =    array.longLow   +  (node2%array.width) * longStep + (longStep/2);
-    double n2_lat =     array.latLow    +   std::floor(node2/array.width) * latStep + (latStep/2);
+    result[0] =  array.longLow + std::floor(id/array.height) * longStep + longStep/2;
+    result[1] =  array.latLow + (id%array.height) * latStep + latStep/2;
+    return;
 
-    uint64_t result = uint64_t(latLongDistance(n1_long, n1_lat, n2_long, n2_lat));
+}
 
-    std::cout << "pos: "<< n1_long << " " << n1_lat << "\t" << "pos: "<< n2_long << " " << n2_lat << "\tdist: " << result <<std::endl;
-    return result;
+uint64_t nodeDistance(AdjacencyArray &array, uint64_t node1, uint64_t node2){
+
+    std::array<double,2> longLat_1;
+    std::array<double,2> longLat_2;
+    nodeIdToLongLat(longLat_1, array, node1);
+    nodeIdToLongLat(longLat_2, array, node2);
+
+    uint64_t result = uint64_t(latLongDistance(longLat_1[0], longLat_1[1], longLat_2[0], longLat_2[1]));
+
+    //std::cout << "pos: "<< n1_long << " " << n1_lat << "\t" << "pos: "<< n2_long << " " << n2_lat << "\tdist: " << result <<std::endl;
+    return result; //result;
 }
 
 void testLatLongDistance(){
@@ -133,31 +140,11 @@ void testLatLongDistance(){
 }
 
 
-void dijkstraResult(std::vector<uint64_t> &path,  uint64_t startPoint, uint64_t endPoint, AdjacencyArray &array){
-    // if one of the two points is on land return instant
-    if(array.nodes.at(startPoint) || array.nodes.at(endPoint)){
-        std::cout << "one point is on land" << std::endl;
-        if(array.nodes.at(startPoint)){
-            std::cout << startPoint << " on land" << std::endl;
-        }
-        if(array.nodes.at(endPoint)){
-            std::cout << endPoint << " on land" << std::endl;
-        }
-        return;
-    }
+void fillMaps(
+    std::map<uint64_t,uint64_t> &distance, std::map<uint64_t,uint64_t> &previous,
+    uint64_t startPoint, uint64_t endPoint, AdjacencyArray &array){
 
-    nodeDistance(array, startPoint, endPoint);
 
-    std::map<uint64_t,uint64_t> distance;
-    std::map<uint64_t,uint64_t> previous;
-    distance.insert({startPoint, 0});
-
-    for (int i = 0; i < array.nodes.size(); i++){
-        if(i != startPoint){
-            distance.insert({i, INT64_MAX});
-            previous.insert({i, INT64_MAX});
-        }
-    }
 
     //auto cmp = [](uint64_t left, uint64_t right, std::map<uint64_t, uint64_t> &dis){return &dis.at(left) < &dis.at(right);};
     typedef std::pair<uint64_t, uint64_t> pqPair;
@@ -170,30 +157,91 @@ void dijkstraResult(std::vector<uint64_t> &path,  uint64_t startPoint, uint64_t 
     uint64_t currTargetNode;
     uint64_t currDistance;
 
-
-
-    while (distance.at(endPoint) == INT64_MAX){
+    uint64_t currTargetDistance = UINT64_MAX;
+    while (true){
         if(pq.empty()){
+            // no node can be expanded anymore
             return;
         }
         currTop = pq.top(); // first element is dist, second in node id
         pq.pop();
         currSourceNode = currTop.second;
+        //std::cout << pq.size() << std::endl;
 
         for(uint64_t currEdgeId = array.offsets.at(currSourceNode); currEdgeId < array.offsets.at(currSourceNode+1); currEdgeId++){
             currTargetNode = array.edges.at(currEdgeId);
             currDistance = distance.at(currSourceNode) + nodeDistance(array, currSourceNode, currTargetNode);
-            std::cout << "Edge: "<< currEdgeId << "\t" << "sNode: "<< currSourceNode << "\ttNode: "  << currTargetNode << std::endl;
-
+            
             if(distance.at(currTargetNode) > currDistance){
+                if(currTargetNode == endPoint){
+                    // save current best distance to endPoint
+                    currTargetDistance = currDistance;
+                }
                 distance.at(currTargetNode) = currDistance;
                 previous.at(currTargetNode) = currSourceNode;
-                std::cout << currDistance << std::endl;
-                pq.push(std::make_pair(currDistance, currTargetNode));
+
+                // do not expand node if it has higher distance than 
+                if(currDistance <= currTargetDistance){
+                    pq.push(std::make_pair(currDistance, currTargetNode));
+                }
             }
         }
     }
+
+    
+
+
 }
+
+
+void generatePath(std::vector<uint64_t> &path,  uint64_t startPoint, uint64_t endPoint, AdjacencyArray &array){
+    std::map<uint64_t,uint64_t> distance;
+    std::map<uint64_t,uint64_t> previous;
+
+    // check if one of the points is on land
+    if(array.nodes.at(startPoint) || array.nodes.at(endPoint)){
+        std::cout << "one point is on land" << std::endl;
+        if(array.nodes.at(startPoint)){
+            std::cout << startPoint << " on land" << std::endl;
+        }
+        if(array.nodes.at(endPoint)){
+            std::cout << endPoint << " on land" << std::endl;
+        }
+        return;
+    }    
+
+
+    // reset map
+    for (int i = 0; i < array.nodes.size(); i++){
+        distance.insert({i, UINT64_MAX});
+        previous.insert({i, UINT64_MAX});
+    }
+    distance.at(startPoint) = 0;
+
+    fillMaps(distance, previous, 1001, 10001, array);
+
+
+    if(distance.at(endPoint) < INT64_MAX){
+        // build up path
+        uint64_t currNode = endPoint;
+        while(currNode != startPoint){
+            currNode = previous.at(currNode);
+            path.push_back(currNode);
+        }
+
+        // print path
+        std::cout << "Path:" << std::endl;
+        for (std::vector<uint64_t>::iterator it = path.begin(); it != path.end(); ++it) {
+            std::cout << *it << " ";
+        }
+        std::cout <<  std::endl;
+        std::cout << "dist: " << distance.at(endPoint)/1000 << "km" << std::endl;
+    }else{
+        std::cout << "no path found" << std::endl;
+    }
+}
+
+
 
 int main(int argc, char** argv) {
     AdjacencyArray adjArray;
@@ -201,19 +249,19 @@ int main(int argc, char** argv) {
     loadAdjacencyArray(adjArray, "data/worldGrid_1415_707.save");
 
     std::vector<uint64_t> result;
-    dijkstraResult(result, 1001, 1002, adjArray);
+    generatePath(result, 1001, 10001, adjArray);
 
-    int counter_one = 0;
-    int counter_zero = 0;
-    for (int i = 0; i < adjArray.nodes.size(); i++){
-        if(adjArray.nodes.at(i) == 0){
-            counter_zero++;
-        }else
-        {
-            counter_one++;
-        }
-    }
-    std::cout << counter_zero << " " << counter_one << " " << counter_one + counter_zero << std::endl;
-    std::cout << adjArray.edges.size() << std::endl;
-    testLatLongDistance();
+    //int counter_one = 0;
+    //int counter_zero = 0;
+    //for (int i = 0; i < adjArray.nodes.size(); i++){
+    //    if(adjArray.nodes.at(i) == 0){
+    //        counter_zero++;
+    //    }else
+    //    {
+    //        counter_one++;
+    //    }
+    //}
+    //std::cout << counter_zero << " " << counter_one << " " << counter_one + counter_zero << std::endl;
+    //std::cout << adjArray.edges.size() << std::endl;
+    //testLatLongDistance();
 }
