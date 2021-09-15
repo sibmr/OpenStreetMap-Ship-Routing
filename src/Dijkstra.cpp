945d077551f05d45b83899ae48121d4ce86c5457
Marcel <marcel.galuschka@web.de>
2021-09-15 16:57:24 +0200

    prepare dijkstra for bidirection

--- src/Dijkstra.cpp
+++ src/Dijkstra.cpp
@@ -68,7 +68,28 @@ class DijkstraSavedEdges: public PathAlgorithm{
         uint64_t startPoint, endPoint, lastCalculatedDistance;
 };
 
-
+/**
+ * @brief Our more efficient implementation of the dijkstra algorithm
+ * Similar implementation to https://github.com/Lesstat/dijkstra-performance-study/
+ */
+class DijkstraBiDirect: public PathAlgorithm{
+    public:
+        DijkstraBiDirect(AdjacencyArray &array);
+        void getPath(std::vector<uint64_t> &path);
+        uint64_t getDist();
+        uint64_t calculateDist(uint64_t startPoint, uint64_t endPoint);
+        void reset();
+    private:
+        uint64_t fillVectors(uint64_t startPoint, uint64_t endPoint);
+        std::vector<uint64_t> distance;
+        std::vector<HeapElement> heap;
+        std::vector<uint64_t> visited;
+        std::vector<uint64_t> prev;
+        AdjacencyArray &adjArray;
+        uint64_t constLngDist;
+        std::vector<uint64_t> constLatDist;
+        uint64_t startPoint, endPoint, lastCalculatedDistance;
+};
 
 
 /**
@@ -227,7 +248,7 @@ void DijkstraImpl::getPath(std::vector<uint64_t> &path){
 
 
 /**
- * @brief Construct a new Dijkstra:: DijkstraImpl object
+ * @brief Construct a new Dijkstra:: DijkstraImpl object use distances from Memory
  * 
  * Initialize datastructures
  * Initialize distances between nodes
@@ -375,7 +396,155 @@ void DijkstraSavedEdges::getPath(std::vector<uint64_t> &path){
     }
     
 }
+/**
+ * @brief Construct a new Dijkstra:: DijkstraImpl object from both sides
+ * 
+ * Initialize datastructures
+ * Initialize distances between nodes
+ * 
+ * @param array 
+ */
+DijkstraBiDirect::DijkstraBiDirect(AdjacencyArray &array) : adjArray(array), prev(array.width*array.height, UINT64_MAX){
+    reset();
+    
+    // calculate distances between nodes:
+    // constLngDist is the distance between nodes with the same longitude but different latitude
+    // constLatDist is the distance between nodes with the same latitude but different logitude
+    // distance between (i,0) and (i,1)
+    constLngDist = nodeDistance(adjArray, 0, 1);
+    // for each constant latitude "ring" around the globe, the distance is different
+    // mirroring is disregarded
+    for(uint64_t i = 0; i<adjArray.height; ++i){
+        // distance between (0,i) and (1,i)
+        constLatDist.push_back(nodeDistance(adjArray, i, adjArray.height+i));
+    }
+}
 
+/**
+ * @brief reset datastructures to prepare for next call to calculateDist
+ */
+void DijkstraBiDirect::reset(){
+    std::vector<uint64_t> initDist(adjArray.width*adjArray.height, UINT64_MAX);
+    distance = std::move(initDist);
+    // no need to reset prev
+    heap.clear();
+}
+
+
+
+/**
+ * @brief efficient dijkstra shortest-path implementation
+ * 
+ * Uses binary Min(Max)-heap for greedy node visitation strategy
+ * 
+ * Saved Distances
+ * 
+ * @param startPoint 
+ * @param endPoint 
+ * @return uint64_t 
+ */
+uint64_t DijkstraBiDirect::calculateDist(uint64_t startPoint_, uint64_t endPoint_){
+
+    startPoint = startPoint_;
+    endPoint = endPoint_;
+
+    // early stop with same start value
+    if(std::find (visited.begin(), visited.end(), endPoint) != visited.end() && distance.at(startPoint) == 0){
+        return distance.at(endPoint);
+    }else{
+        reset();
+    }
+
+    heap.push_back(HeapElement{startPoint, UINT64_MAX, 0});
+
+    std::make_heap(heap.begin(), heap.end());
+
+    HeapElement front;
+    while(true){
+        if(heap.empty()){
+            return UINT64_MAX;
+        }
+
+        std::pop_heap(heap.begin(), heap.end());
+        front = heap.back();
+        heap.pop_back();
+
+        // avoid duplicate nodes
+        if(front.heuristic_dist >= distance.at(front.nodeIdx)){
+            continue;
+        }
+
+        distance.at(front.nodeIdx) = front.heuristic_dist;
+        prev.at(front.nodeIdx) = front.prev;
+        visited.push_back(front.nodeIdx);
+
+        for(uint64_t currEdgeId = adjArray.offsets.at(front.nodeIdx); currEdgeId < adjArray.offsets.at(front.nodeIdx+1); currEdgeId++){
+            uint64_t neighborIdx = adjArray.edges.at(currEdgeId);
+
+            // absolute difference of unsigned int
+            uint64_t idxDiff = neighborIdx<front.nodeIdx ? front.nodeIdx-neighborIdx : neighborIdx-front.nodeIdx;
+
+            // choose length of edge from precalculated lengths
+            //uint64_t edgeDist = (idxDiff < 2) ? constLngDist : constLatDist.at(neighborIdx%adjArray.height);
+            uint64_t edgeDist = adjArray.distances.at(currEdgeId);
+
+            uint64_t newNeighborDist = front.heuristic_dist + edgeDist;
+            uint64_t oldNeighborDist = distance.at(neighborIdx);
+
+            if(newNeighborDist<oldNeighborDist){
+                // do not update distance array: only update for distances that are final
+                // distance.at(neighborIdx) = newNeighborDist;
+                heap.push_back(HeapElement{neighborIdx, front.nodeIdx, newNeighborDist});
+                std::push_heap(heap.begin(), heap.end());
+            }
+        }
+
+        if(front.nodeIdx == endPoint){
+            lastCalculatedDistance = distance.at(front.nodeIdx);
+            return lastCalculatedDistance;
+        }
+
+    }
+
+}
+
+/**
+ * @brief returns distance of last call to calculateDist
+ * 
+ * @return uint64_t 
+ */
+uint64_t DijkstraBiDirect::getDist(){
+    return lastCalculatedDistance;
+}
+
+/**
+ * @brief retrieve path calculated by the last call to calculateDist
+ * 
+ * @param path 
+ */
+void DijkstraBiDirect::getPath(std::vector<uint64_t> &path){
+    if(distance.at(endPoint) < UINT64_MAX){
+        // build up path
+        uint64_t currNode = endPoint;
+        while(currNode != startPoint){
+            currNode = prev.at(currNode);
+            path.push_back(currNode);
+        }
+
+        // print path
+        std::cout << "Path:" << std::endl;
+        for (std::vector<uint64_t>::iterator it = path.begin(); it != path.end(); ++it) {
+            std::cout << *it << " ";
+        }
+        std::cout <<  std::endl;
+        std::cout << "dist: " << distance.at(endPoint)/1000 << "km" << std::endl;
+    }else{
+        std::cout << "no path found" << std::endl;
+        //path.push_back(startPoint);
+        //path.push_back(endPoint);
+    }
+    
+}
 
 
 void test() {
