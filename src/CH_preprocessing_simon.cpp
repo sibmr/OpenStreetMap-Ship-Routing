cf9d9fc16dc38d86678a33d181f314c5d7d2ec5d
sibmr <simon.bihlmaier@gmail.com>
2021-09-24 15:56:48 +0200

    add shorter path check for shortcut creation

--- src/CH_preprocessing_simon.cpp
+++ src/CH_preprocessing_simon.cpp
@@ -2,6 +2,7 @@
 #include <numeric>
 
 #include "shortestPathUtils.cpp"
+#include "Dijkstra_simon.cpp"
 
 struct Edge
 {
@@ -9,7 +10,7 @@ struct Edge
     uint64_t v1;
     uint64_t v2;
     uint64_t edgeDistance;
-    std::vector<uint64_t> removedEdgeIds;
+    std::vector<uint64_t> shortcutPathEdges;
 };
 
 std::vector<Edge> copyEdgeVector(std::vector<Edge> &edgeVector){
@@ -20,13 +21,21 @@ std::vector<Edge> copyEdgeVector(std::vector<Edge> &edgeVector){
             .v1 = edge.v1,
             .v2 = edge.v2,
             .edgeDistance = edge.edgeDistance,
-            .removedEdgeIds = std::vector<uint64_t>(edge.removedEdgeIds)
+            .shortcutPathEdges = std::vector<uint64_t>(edge.shortcutPathEdges)
         });
     }
     return copy;
 }
 
-int getPathInput(int argc, char** argv, std::string &out_path){
+std::vector<std::vector<uint64_t>> createVectorMatrix(uint64_t size_i, uint64_t size_j){
+    std::vector<std::vector<uint64_t>> matrix(size_i);
+    for(uint64_t i = 0; i<size_i; ++i){
+        matrix.at(i).resize(size_j);
+    }
+    return matrix;
+}
+
+int getFilePathInput(int argc, char** argv, std::string &out_path){
     if(argc > 1){
         out_path = std::string(argv[1]);
     }else{
@@ -50,8 +59,22 @@ void addEdgeIds(AdjacencyArray &adjArray){
     }
 }
 
+void nodeToEdgeIdPath(AdjacencyArray &adjArray, std::vector<uint64_t> &in_nodeIdPath, std::vector<uint64_t> &out_edgeIdPath){
+    uint64_t prev_nodeId = in_nodeIdPath.at(0);
+    for(uint64_t nodeIndex = 1; nodeIndex<in_nodeIdPath.size(); ++nodeIndex){
+        uint64_t nodeId = in_nodeIdPath.at(nodeIndex);
+        for(uint64_t currEdgeIndex = adjArray.offsets.at(prev_nodeId); currEdgeIndex < adjArray.offsets.at(prev_nodeId+1); ++currEdgeIndex){
+            uint64_t nextNode = adjArray.edges.at(currEdgeIndex);
+            if(nextNode == nodeId){
+                out_edgeIdPath.push_back(adjArray.edgeIds.at(currEdgeIndex));
+            }
+        }
+    }
+}
+
 void extractAllEdges(AdjacencyArray &adjArray, std::vector<Edge> &out_allEdges){
     uint64_t edgeIdCounter = 0;
+    uint64_t noEdgesCounter = 0;
     for(uint64_t nodeId = 0; nodeId < adjArray.width*adjArray.height; ++nodeId){
         if(!adjArray.nodes.at(nodeId)){
             for(uint64_t currEdgeIndex = adjArray.offsets.at(nodeId); currEdgeIndex < adjArray.offsets.at(nodeId+1); ++currEdgeIndex){
@@ -62,12 +85,17 @@ void extractAllEdges(AdjacencyArray &adjArray, std::vector<Edge> &out_allEdges){
                         .v1 = nodeId, 
                         .v2 = adjacentId, 
                         .edgeDistance = adjArray.distances.at(currEdgeIndex),
-                        .removedEdgeIds = {}
+                        .shortcutPathEdges = {}
                         }
                 );
             }
+
+            if(adjArray.offsets.at(nodeId) == adjArray.offsets.at(nodeId+1)){
+                noEdgesCounter++;
+            }
         }
     }
+    std::cout << "Number of active nodes without edges: " << noEdgesCounter << "\n";
 }
 
 bool isNodeAdjacentToSet(AdjacencyArray &adjArray, uint64_t idToCheck, std::vector<uint64_t> &idSet){
@@ -89,7 +117,7 @@ void fillContractionSet(
     std::vector<uint64_t> &out_newContractions
     ){
 
-    uint64_t numDraws = 10000;
+    uint64_t numDraws = 200000;
     uint64_t maxNodeId = isContracted.size();
     
     uint64_t nodeIdx = 0;
@@ -117,7 +145,7 @@ void fillContractionSet(
 
 int main(int argc, char** argv){
     std::string path;
-    if(getPathInput(argc, argv, path)==0){
+    if(getFilePathInput(argc, argv, path)==0){
         return 0;
     }
 
@@ -147,6 +175,8 @@ int main(int argc, char** argv){
     while((workArray.width*workArray.height - std::accumulate(workArray.nodes.begin(), workArray.nodes.end(), 0)) > 20000){
         std::cout << "round " << round++ << "\n";
 
+        Dijkstra::Dijkstra dijkstra(workArray);
+        
         std::vector<Edge> shortcutEdges;
         std::vector<uint64_t> removedEdgeIndices;
         std::vector<bool> isEdgeRemoved(workArray.edges.size(), false);
@@ -185,6 +215,43 @@ int main(int argc, char** argv){
                 }
             }
 
+            //calculate distances
+            std::vector<std::vector<uint64_t>> distanceUWMatrix = createVectorMatrix(adjacentIds.size(), adjacentIds.size());
+            for(uint64_t uIndex = 0; uIndex<adjacentIds.size(); ++uIndex){
+                uint64_t uId = adjacentIds.at(uIndex);
+                dijkstra.reset();
+                dijkstra.disableNode(contractedNodeId); // disable contracted node
+                if(uIndex+1<adjacentIds.size()){
+                    dijkstra.calculateDist(uId, adjacentIds.at(uIndex+1)); // set correct start point
+                } 
+                for(uint64_t wIndex = uIndex+1; wIndex<adjacentIds.size(); ++wIndex){
+                    uint64_t wId = adjacentIds.at(wIndex);
+                    uint64_t distance = dijkstra.calculateDist(wId); // only query with end point (one-to-many)
+                    distanceUWMatrix.at(uIndex).at(wIndex) = distance;
+                    distanceUWMatrix.at(wIndex).at(uIndex) = distance;
+                }
+            }
+            std::vector<uint64_t> distanceUV(adjacentIds.size()); // for calculating uvw
+            std::vector<uint64_t> shortcutPathPart;
+            std::vector<std::vector<uint64_t>> shortcutEdgePathPart(adjacentIds.size());
+            if(adjacentIds.size() > 0){
+                dijkstra.reset();
+                dijkstra.calculateDist(contractedNodeId, adjacentIds.at(0)); // set correct start point
+                for(uint64_t uIndex = 0; uIndex<adjacentIds.size(); ++uIndex){
+                    uint64_t uId = adjacentIds.at(uIndex);
+                    uint64_t distance = dijkstra.calculateDist(uId); // only query with end point (one-to-many)
+                    distanceUV.at(uIndex) = distance;
+                    shortcutPathPart.clear();
+                    dijkstra.getPath(shortcutPathPart); // get shortest path from v to u
+                    nodeToEdgeIdPath(workArray, shortcutPathPart, shortcutEdgePathPart.at(uIndex));
+                }
+            }
+            else{
+                std::cout << "empty node: " << contractedNodeId << "\n";
+            }
+            
+            
+
             // insert shortcuts in both directions
             for(uint64_t i = 0; i<adjacentIds.size(); ++i){
                 for(uint64_t j = 0; j<adjacentIds.size(); ++j){
@@ -193,22 +260,34 @@ int main(int argc, char** argv){
                     uint64_t startNode = adjacentIds.at(i);
                     uint64_t endNode = adjacentIds.at(j);
                     
-                    // TODO: check if shortcut is necessary between start and end node
-                    
-                    // add shortcut to list
-                    uint64_t edgeDistance = workArray.distances.at(edgeIndices.at(i)) + workArray.distances.at(edgeIndices.at(j));
-                    uint64_t shortcutId = allEdges.size();
-                    std::vector<uint64_t> replacedEdges{edgeIds.at(i), edgeIds.at(j)};
-                    Edge newEdge{
-                        .edgeId=shortcutId,
-                        .v1=startNode, 
-                        .v2=endNode, 
-                        .edgeDistance=edgeDistance, 
-                        .removedEdgeIds=replacedEdges
-                        };
-                    allEdges.push_back(newEdge);
-                    shortcutEdges.push_back(newEdge);
-                    
+                    uint64_t distanceUW = distanceUWMatrix.at(i).at(j);
+                    uint64_t distanceUVW = distanceUV.at(i) + distanceUV.at(j); // i is u, j is w
+
+                    // if replaced edges are potentially part of a shortest path, then add shortcut
+                    if(distanceUW >= distanceUVW){
+                        // add shortcut to list
+                        // use distances of edges (contractedNodeId, otherNodeId) - instead of (otherNodeId, contractedNodeId)
+                        uint64_t edgeDistance = distanceUVW;
+                        uint64_t shortcutId = allEdges.size();
+                        std::vector<uint64_t> edgePathUVW;
+                        std::vector<uint64_t> edgePathVU = shortcutEdgePathPart.at(i);
+                        std::vector<uint64_t> edgePathVW = shortcutEdgePathPart.at(j);
+                        for(uint64_t pathIndex = edgePathVU.size()-1; pathIndex >= 0 && pathIndex < edgePathVU.size(); --pathIndex){
+                            edgePathUVW.push_back(edgePathVU.at(pathIndex));
+                        }
+                        for(uint64_t pathIndex = 0; pathIndex < edgePathVW.size(); --pathIndex){
+                            edgePathUVW.push_back(edgePathVW.at(pathIndex));
+                        }
+                        Edge newEdge{
+                            .edgeId=shortcutId,
+                            .v1=startNode, 
+                            .v2=endNode, 
+                            .edgeDistance=edgeDistance, 
+                            .shortcutPathEdges=edgePathUVW
+                            };
+                        allEdges.push_back(newEdge);
+                        shortcutEdges.push_back(newEdge);
+                    }
                 }
             }
 
